f6ba9cdb0424ef51eed40e86d6c81dca67cb8c8e
Your Name <you@example.com>
2022-06-29 16:43:26 +0800

    js

--- template/default/statics/js/demo.js
+++ template/default/statics/js/demo.js
@@ -65,10 +65,6 @@
 
 
 
-        function formatTime(t) {
-            var d = new Date(t);
-            return d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-        }
 
         function createUUID() {
             var dt = new Date().getTime();
---
+++ template/default/statics/js/func.js
@@ -0,0 +1,4 @@
+function formatTime(t) {
+    var d = new Date(t);
+    return d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+}
