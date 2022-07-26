d2056a13e235e9f039812fbfc3552cb180084954
Your Name <you@example.com>
2022-07-26 19:01:25 +0800

    update

--- clearing/clear.go
+++ clearing/clear.go
@@ -70,6 +70,7 @@ func (c *clearing) updateOrder(side orders.OrderSide) error {
 	}
 	order.FinishedQty = d(order.FinishedQty).Add(c.trade_qty).String()
 	order.TradeAmount = d(order.TradeAmount).Add(c.trade_amount).String()
+	order.TradeSymbol = c.symbol
 
 	//todo 一些必要的边界值检查
 
